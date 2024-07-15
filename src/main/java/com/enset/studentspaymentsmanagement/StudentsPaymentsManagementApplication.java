7333642839c780249071a714e10aaa3758d19629
Antra Mohammed <ANTRA@192.168.1.8>
2024-07-15 22:04:52 +0100

    deployment automation

--- src/main/java/com/enset/studentspaymentsmanagement/StudentsPaymentsManagementApplication.java
+++ src/main/java/com/enset/studentspaymentsmanagement/StudentsPaymentsManagementApplication.java
@@ -18,17 +18,22 @@ public class StudentsPaymentsManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentsPaymentsManagementApplication.class, args);
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("spring.datasource.url", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_URL")));
-        System.setProperty("spring.datasource.username", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_USERNAME")));
-        System.setProperty("spring.datasource.password", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_PASSWORD")));
-        System.setProperty("spring.mail.username", Objects.requireNonNull(dotenv.get("SPRING_MAIL_USERNAME")));
-        System.setProperty("spring.mail.password", Objects.requireNonNull(dotenv.get("SPRING_MAIL_PASSWORD")));
-        System.setProperty("jwt.secret", Objects.requireNonNull(dotenv.get("JWT_SECRET")));
-        System.setProperty("application.mailing.frontend.activation-url", Objects.requireNonNull(dotenv.get("APPLICATION_MAILING_FRONTEND_ACTIVATION_URL")));
+        setSystemProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+        setSystemProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        setSystemProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        setSystemProperty("spring.mail.username", dotenv.get("SPRING_MAIL_USERNAME"));
+        setSystemProperty("spring.mail.password", dotenv.get("SPRING_MAIL_PASSWORD"));
+        setSystemProperty("jwt.secret", dotenv.get("JWT_SECRET"));
+        setSystemProperty("application.mailing.frontend.activation-url", dotenv.get("APPLICATION_MAILING_FRONTEND_ACTIVATION_URL"));
 
 
     }
 
+    private static void setSystemProperty(String key, String value) {
+        if (System.getProperty(key) == null && value != null) {
+            System.setProperty(key, value);
+        }
+    }
 
 }
 
